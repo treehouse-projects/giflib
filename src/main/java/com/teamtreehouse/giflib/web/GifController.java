6140743407e2a0436a61feac4aea41c3f54cd6bc
Chris Ramacciotti <cjramey@gmail.com>
2015-10-07 18:06:15 -0500

    Added the Thymeleaf library and a homepage template

--- src/main/java/com/teamtreehouse/giflib/web/GifController.java
+++ src/main/java/com/teamtreehouse/giflib/web/GifController.java
@@ -2,13 +2,11 @@ package com.teamtreehouse.giflib.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class GifController {
     @RequestMapping("/")
-    @ResponseBody
     public String listGifs() {
-        return "List of all the GIFs!";
+        return "home";
     }
 }
