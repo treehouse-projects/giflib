15f53157e3e0bd9e2cc6a1e8c2b8d1337a8b6854
Chris Ramacciotti <cjramey@gmail.com>
2015-10-08 17:15:13 -0500

    Added Gif class & GIF detail page, passing an arbitrary Gif object

---
+++ src/main/java/com/teamtreehouse/giflib/model/gif.java
@@ -0,0 +1,49 @@
+package com.teamtreehouse.giflib.model;
+
+import java.time.LocalDate;
+
+public class Gif {
+    private String name;
+    private LocalDate dateUploaded;
+    private String username;
+    private boolean favorite;
+
+    public Gif(String name, LocalDate dateUploaded, String username, boolean favorite) {
+        this.name = name;
+        this.dateUploaded = dateUploaded;
+        this.username = username;
+        this.favorite = favorite;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getDateUploaded() {
+        return dateUploaded;
+    }
+
+    public void setDateUploaded(LocalDate dateUploaded) {
+        this.dateUploaded = dateUploaded;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public boolean isFavorite() {
+        return favorite;
+    }
+
+    public void setFavorite(boolean favorite) {
+        this.favorite = favorite;
+    }
+}
--- src/main/java/com/teamtreehouse/giflib/web/GifController.java
+++ src/main/java/com/teamtreehouse/giflib/web/GifController.java
@@ -1,12 +1,23 @@
 package com.teamtreehouse.giflib.web;
 
+import com.teamtreehouse.giflib.model.Gif;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.time.LocalDate;
 
 @Controller
 public class GifController {
     @RequestMapping("/")
     public String listGifs() {
         return "home";
+    }
+
+    @RequestMapping("/gif")
+    public String gifDetails(Model model) {
+        Gif gif = new Gif("compiler-bot", LocalDate.of(2015, 2, 13), "Chris Ramacciotti", true);
+        model.addAttribute("gif",gif);
+        return "gif-details";
     }
 }
