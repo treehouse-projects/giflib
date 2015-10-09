06ca4f2466ab9230e52d758cd2ac07d816b2f7cc
Chris Ramacciotti <cjramey@gmail.com>
2015-10-09 14:42:22 -0500

    Configured repository as a Spring component to be injected into the controller with @Autowire

--- src/main/java/com/teamtreehouse/giflib/data/GifRepository.java
+++ src/main/java/com/teamtreehouse/giflib/data/GifRepository.java
@@ -1,18 +1,33 @@
 package com.teamtreehouse.giflib.data;
 
 import com.teamtreehouse.giflib.model.Gif;
+import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.List;
 
+@Component
 public class GifRepository {
-    public static final List<Gif> ALL_GIFS = Arrays.asList(
-        new Gif("android-explosion", LocalDate.of(2015,2,13), "", false),
-        new Gif("ben-and-mike", LocalDate.of(2015,10,30), "", true),
-        new Gif("book-dominos", LocalDate.of(2015,9,15), "", false),
-        new Gif("compiler-bot", LocalDate.of(2015,2,13), "", true),
-        new Gif("cowboy-coder", LocalDate.of(2015,2,13), "", false),
-        new Gif("infinite-andrew", LocalDate.of(2015,8,23), "", true)
+    private static final List<Gif> ALL_GIFS = Arrays.asList(
+        new Gif("android-explosion", LocalDate.of(2015,2,13), "Chris Ramacciotti", false),
+        new Gif("ben-and-mike", LocalDate.of(2015,10,30), "Ben Jakuben", true),
+        new Gif("book-dominos", LocalDate.of(2015,9,15), "Craig Dennis", false),
+        new Gif("compiler-bot", LocalDate.of(2015,2,13), "Ada Lovelace", true),
+        new Gif("cowboy-coder", LocalDate.of(2015,2,13), "Grace Hopper", false),
+        new Gif("infinite-andrew", LocalDate.of(2015,8,23), "Marissa Mayer", true)
     );
+
+    public List<Gif> getAllGifs(){
+        return ALL_GIFS;
+    }
+
+    public Gif findByName(String name) {
+        for(Gif gif : ALL_GIFS) {
+            if(gif.getName().equals(name)) {
+                return gif;
+            }
+        }
+        return null;
+    }
 }
--- src/main/java/com/teamtreehouse/giflib/web/GifController.java
+++ src/main/java/com/teamtreehouse/giflib/web/GifController.java
@@ -2,24 +2,29 @@ package com.teamtreehouse.giflib.web;
 
 import com.teamtreehouse.giflib.data.GifRepository;
 import com.teamtreehouse.giflib.model.Gif;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.time.LocalDate;
+import java.util.List;
 
 @Controller
 public class GifController {
+    @Autowired
+    private GifRepository gifRepository;
+
     @RequestMapping("/")
     public String listGifs(Model model) {
-        model.addAttribute("gifs", GifRepository.ALL_GIFS);
+        List<Gif> allGifs = gifRepository.getAllGifs();
+        model.addAttribute("gifs", allGifs);
         return "home";
     }
 
     @RequestMapping("/gif/{name}")
     public String gifDetails(Model model, @PathVariable String name) {
-        Gif gif = new Gif(name, LocalDate.of(2015, 2, 13), "Chris Ramacciotti", true);
+        Gif gif = gifRepository.findByName(name);
         model.addAttribute("gif",gif);
         return "gif-details";
     }
